722a2c6f9f4a2c0fc97f1d494493df31c9b50d42
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:20 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "virtual_hub_bgp_connections" {
-  description = "All virtual_hub_bgp_connection resources"
-  value       = azurerm_virtual_hub_bgp_connection.virtual_hub_bgp_connections
-}
 output "virtual_hub_bgp_connections_name" {
-  description = "List of name values across all virtual_hub_bgp_connections"
-  value       = [for k, v in azurerm_virtual_hub_bgp_connection.virtual_hub_bgp_connections : v.name]
+  description = "Map of name values across all virtual_hub_bgp_connections, keyed the same as var.virtual_hub_bgp_connections"
+  value       = { for k, v in azurerm_virtual_hub_bgp_connection.virtual_hub_bgp_connections : k => v.name }
 }
 output "virtual_hub_bgp_connections_peer_asn" {
-  description = "List of peer_asn values across all virtual_hub_bgp_connections"
-  value       = [for k, v in azurerm_virtual_hub_bgp_connection.virtual_hub_bgp_connections : v.peer_asn]
+  description = "Map of peer_asn values across all virtual_hub_bgp_connections, keyed the same as var.virtual_hub_bgp_connections"
+  value       = { for k, v in azurerm_virtual_hub_bgp_connection.virtual_hub_bgp_connections : k => v.peer_asn }
 }
 output "virtual_hub_bgp_connections_peer_ip" {
-  description = "List of peer_ip values across all virtual_hub_bgp_connections"
-  value       = [for k, v in azurerm_virtual_hub_bgp_connection.virtual_hub_bgp_connections : v.peer_ip]
+  description = "Map of peer_ip values across all virtual_hub_bgp_connections, keyed the same as var.virtual_hub_bgp_connections"
+  value       = { for k, v in azurerm_virtual_hub_bgp_connection.virtual_hub_bgp_connections : k => v.peer_ip }
 }
 output "virtual_hub_bgp_connections_virtual_hub_id" {
-  description = "List of virtual_hub_id values across all virtual_hub_bgp_connections"
-  value       = [for k, v in azurerm_virtual_hub_bgp_connection.virtual_hub_bgp_connections : v.virtual_hub_id]
+  description = "Map of virtual_hub_id values across all virtual_hub_bgp_connections, keyed the same as var.virtual_hub_bgp_connections"
+  value       = { for k, v in azurerm_virtual_hub_bgp_connection.virtual_hub_bgp_connections : k => v.virtual_hub_id }
 }
 output "virtual_hub_bgp_connections_virtual_network_connection_id" {
-  description = "List of virtual_network_connection_id values across all virtual_hub_bgp_connections"
-  value       = [for k, v in azurerm_virtual_hub_bgp_connection.virtual_hub_bgp_connections : v.virtual_network_connection_id]
+  description = "Map of virtual_network_connection_id values across all virtual_hub_bgp_connections, keyed the same as var.virtual_hub_bgp_connections"
+  value       = { for k, v in azurerm_virtual_hub_bgp_connection.virtual_hub_bgp_connections : k => v.virtual_network_connection_id }
 }
 
